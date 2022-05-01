cc23fbfd205263151e02e2c3bcecd93335405432
Manuel Cerezo <cerezomazon53@gmail.com>
2022-05-02 01:57:30 +0200

    tonterias2

--- Spark/server.py
+++ Spark/server.py
@@ -4,6 +4,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from pyspark.sql.session import SparkSession
 from textblob import TextBlob
 
+
 #Inicializacion de Contexto
 sc = SparkContext(appName="SERVER")
 sc.setLogLevel("ERROR") ##-> Ocultar warnings
@@ -11,13 +12,12 @@ ssc = StreamingContext(sc,5) #lectura cada 3s
 spark = SparkSession(sc)
 
 lines = ssc.socketTextStream("localhost",12345)
-#lines2 = spark.readStream.format("socket").option("host", "localhost").option("port", 12345).load()
+# lines2 = spark.readStream.format("socket").option("host", "localhost").option("port", 12345).load()
 #df = spark.createDataFrame(['url','news','date'],str(lines))
 
-
 #===== Procesamiento de los datos =======
-
-lines.pprint()
+words = lines.flatMap(lambda x : x.split(' '))
+words.pprint()
 
 # lines.window(5,5).map(lambda x : vader_analyzer.polarity_scores(x.split(';')[1])['compound']).pprint() #vader
 # lines.window(5,5).map(lambda x: TextBlob(x.split(';')[1]).sentiment.polarity).pprint() #TextBlob
