4c1c65228735baa7395e03b5497c82a5f1facdcf
MiguelH <mherenciagdc@gmail.com>
2022-05-03 20:16:58 +0200

    Importacion librerias, creacion df

--- Spark/server.py
+++ Spark/server.py
@@ -1,10 +1,13 @@
 import requests
-from typing import Text
 from pyspark import Row, SparkContext
 from pyspark.streaming import StreamingContext
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from pyspark.sql.session import SparkSession
 from textblob import TextBlob
+from pyspark.sql.functions import udf
+from pyspark.sql.types import FloatType
+from pyspark.sql.functions import *
+
 
 #Inicializacion de Contexto1
 sc = SparkContext(appName="SERVER")
@@ -12,12 +15,13 @@ sc.setLogLevel("ERROR") ##-> Ocultar warnings
 spark = SparkSession(sc)
 vader_analyzer = SentimentIntensityAnalyzer()
 
-
 #Inicializacion de Contexto2
 ssc = StreamingContext(sc,1) #lectura cada 3s
 lines = ssc.socketTextStream("localhost",12345)
 
 
+apply_vader = udf(lambda x: vader_analyzer.polarity_scores(x)['compound'],FloatType())
+apply_textBlob = udf(lambda x: TextBlob(x).sentiment.polarity,FloatType())
 
 def data_serialize(rdd):
     global spark
@@ -25,14 +29,15 @@ def data_serialize(rdd):
     print(type(rdd))
     #print(rdd.collect())
     df = rdd.toDF(['fuente','url','notice','notice-date','process-date'])
-    df.show(5)
+    df = df.withColumn("notice", apply_vader(col('vader-polarity')))
+    df = df.withColumn("notice", apply_textBlob(col('textBlob-polarity')))
     #df = spark.createDataFrame(['data0','data1','data2','data3','data4'],rdd[0])
     #Creacion de dataframe de ventana de trabajo
     # rowRdd = rdd.map(lambda x: Row( fuente = x[0],url=x[1],news = x[2],notice_date=x[3],\
     #     vader_polarity = vader_analyzer.polarity_scores(x[2])['compound']\
     #         ,textblob_polarity = TextBlob(x[2]).sentiment.polarity,\
     #             process_time = x[4]))
-
+    df.show(6)
     # df = spark.createDataFrame(rowRdd) #Almacenamos los datos temporalmente
     # df.createOrReplaceTempView("cryptonews")
     # #df.show(5)
