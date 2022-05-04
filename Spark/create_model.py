fd2cec8cc6b929adc02ec4ee32398df67498415b
MiguelH <mherenciagdc@gmail.com>
2022-05-04 20:24:05 +0200

    Limpiando codigo

--- Spark/create_model.py
+++ Spark/create_model.py
@@ -1,3 +1,10 @@
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.sql.functions import col, lit
+from pyspark.ml.feature import HashingTF, IDF
+from pyspark.ml.feature import StopWordsRemover
+from pyspark.ml.feature import Tokenizer
+
 import findspark
 findspark.init()
 import pyspark
@@ -8,15 +15,13 @@ spark = SparkSession(sc)
 #read the dataset
 df=spark.read.csv('data.csv',inferSchema=True,header=True)
 
-from pyspark.ml.feature import Tokenizer
 tokenization=Tokenizer(inputCol='news',outputCol='tokens')
 tokenized_df=tokenization.transform(df)
 
-from pyspark.ml.feature import StopWordsRemover
 stopword_removal=StopWordsRemover(inputCol='tokens',outputCol='refined_tokens')
 refined_df=stopword_removal.transform(tokenized_df)
 
-from pyspark.ml.feature import HashingTF, IDF
+
 
 hashingTF = HashingTF(inputCol="refined_tokens", outputCol="rawFeatures", numFeatures=20)
 featurizedData = hashingTF.transform(refined_df)
@@ -25,24 +30,14 @@ idf = IDF(inputCol="rawFeatures", outputCol="features")
 idfModel = idf.fit(featurizedData)
 rescaledData = idfModel.transform(featurizedData)
 
-from pyspark.sql.functions import col, lit
-
 column = 'final_manual_labelling'
 
 rescaledData = rescaledData.withColumn(column, col(column) + lit(1))
-
-
 train, test = rescaledData.randomSplit([0.7, 0.3], seed = 2018)
-print("Training Dataset Count: " + str(train.count()))
-print("Test Dataset Count: " + str(test.count()))
-
-from pyspark.ml.classification import RandomForestClassifier
 
 rf = RandomForestClassifier(featuresCol = 'features', labelCol = 'final_manual_labelling')
 rfModel = rf.fit(train)
 predictions = rfModel.transform(test)
-
-predictions.select("final_manual_labelling", "prediction", "probability").show(10)
 
 true_postives = predictions[(predictions.final_manual_labelling == 2) & (predictions.prediction == 2)].count()
 true_negatives = predictions[(predictions.final_manual_labelling == 0) & (predictions.prediction == 0)].count()
@@ -53,14 +48,9 @@ false_postives = predictions[(predictions.final_manual_labelling == 2) & (predic
 false_negatives = predictions[(predictions.final_manual_labelling == 0) & (predictions.prediction != 0)].count()
 false_neutral = predictions[(predictions.final_manual_labelling == 1) & (predictions.prediction != 1)].count()
 
-print(true_postives, true_postives, true_neutral)
-print(false_postives, false_negatives, false_neutral)
-
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-
 evaluator = MulticlassClassificationEvaluator(labelCol="final_manual_labelling", predictionCol="prediction")
 accuracy = evaluator.evaluate(predictions)
-print("Accuracy = %s" % (accuracy))
-print("Test Error = %s" % (1.0 - accuracy))
 
 rfModel.write().save("./Model_RF_V1")
+
+print("Creacion del modelo terminado")
--- Spark/server.py
+++ Spark/server.py
@@ -1,6 +1,5 @@
-from concurrent.futures import process
 import requests
-from pyspark import Row, SparkContext
+from pyspark import  SparkContext
 from pyspark.streaming import StreamingContext
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from pyspark.sql.session import SparkSession
@@ -33,46 +32,19 @@ from pyspark.ml.feature import HashingTF, IDF
 from pyspark.ml.classification import RandomForestClassificationModel
 
 
-rf_model = RandomForestClassificationModel.load("./Spark/Model_RF_V1")
+rf_model = RandomForestClassificationModel.load("./Model_RF_V1")
  
 # Para que funcione la clase es necesario que se cree primero 
 # el modelo contenido en RandomForest.ipnb
 
-def predict(data):
-    df1 = spark.createDataFrame([(data,)], ["news"])
-    tokenization=Tokenizer(inputCol='news',outputCol='tokens')
-    tokenized_df=tokenization.transform(df1)
-    stopword_removal=StopWordsRemover(inputCol='tokens',outputCol='refined_tokens')
-    refined_df=stopword_removal.transform(tokenized_df)
-    hashingTF = HashingTF(inputCol="refined_tokens", outputCol="rawFeatures", numFeatures=20)
-    featurizedData = hashingTF.transform(refined_df)
-
-    idf = IDF(inputCol="rawFeatures", outputCol="features")
-    idfModel = idf.fit(featurizedData)
-    rescaledData = idfModel.transform(featurizedData)
-    predictions = rf_model.transform(rescaledData)
-    probabilidad = predictions.head().probability
-    if probabilidad[0] > (probabilidad[1] and probabilidad[2]):#negativo
-        #print('negativo')
-        probabilidad = probabilidad[0]*-1
-    elif probabilidad[1] > (probabilidad[0] and probabilidad[2]):#neutro
-        #print('neutro')
-        probabilidad = 1 - probabilidad[1]
-    elif probabilidad[2] >(probabilidad[0] and probabilidad[1]):#positivo
-        #print('positivo')
-        probabilidad = probabilidad[2]
-    print(float(probabilidad))
-    print("siguiente")
-    return float(probabilidad)
-
-
 #Inicializacion de Funciones UDF
 apply_vader = udf(lambda x: vader_analyzer.polarity_scores(x)['compound'],FloatType())
 apply_textBlob = udf(lambda x: TextBlob(x).sentiment.polarity,FloatType())
+apply_cast_mymodel = udf(lambda x: x-1,FloatType())
 #apply_mymodel = udf(lambda x: predict(x),FloatType())
 
 def apply_myModel(df):
-    tokenization=Tokenizer(inputCol='_3',outputCol='tokens')
+    tokenization=Tokenizer(inputCol='notice',outputCol='tokens')
     tokenized_df=tokenization.transform(df)
     stopword_removal=StopWordsRemover(inputCol='tokens',outputCol='refined_tokens')
     refined_df=stopword_removal.transform(tokenized_df)
@@ -87,51 +59,42 @@ def apply_myModel(df):
     return predictions
 
 
-def data_serialize(rdd):    
-    df = rdd.toDF()
+def data_serialize(rdd): 
 
-    df = df.withColumn("vader_polarity", apply_vader(col('_3')))
-    df = df.withColumn("textBlob_polarity", apply_textBlob(col('_3')))
+    df = rdd.toDF(['fuente','url','notice','notice_date','process_date'])
+    df = df.withColumn("vader_polarity", apply_vader(col('notice')))
+    df = df.withColumn("textBlob_polarity", apply_textBlob(col('notice')))
     df = apply_myModel(df)
-    df = df.withColumn("textBlob_polarity", apply_textBlob(col('_3')))
-    df = df.withColumn("myModel_polarity", col('prediction'))
+    df = df.withColumn("textBlob_polarity", apply_textBlob(col('notice')))
+    df = df.withColumn("myModel_prediction", apply_cast_mymodel(col('prediction')))
 
-    df = df.drop(*("tokens", "refined_tokens", "rawFeatures", "features", "rawPrediction", "prediction"))
+    df = df.drop(*("tokens", "refined_tokens", "rawFeatures", "features", "rawPrediction", "prediction","probability"))
 
-    df.show(2)
-    #rowRdd = rdd.map(lambda x: Row(fuente = x[0], url = x[1], notice = x[2], notice_date = x[3], process_date = x[4], myModel = predict(x[2])))
-    #df1 = spark.createDataFrame(rowRdd)
-    #df1.show(2)
-    # df = rdd.toDF(['fuente','url','notice','notice_date','process_date'])
-    # df = df.withColumn("vader_polarity", apply_vader(col('notice')))
-    # df = df.withColumn("textBlob_polarity", apply_textBlob(col('notice')))
-    # df = df.withColumn("mymodel_polarity", apply_mymodel(col('notice')))
-
-    # df.createOrReplaceTempView("cryptonews")
-    # df.show(2)
-    #send_to_server()
+    df.createOrReplaceTempView("cryptonews")
+    df.show(20)
+    send_to_server()
 
     
 
-# def send_to_server():
-#     global spark
-#     data_to_sent = {}
+def send_to_server():
+    global spark
+    data_to_sent = {}
 
-#     ## obtener datos del dataframe
-#     ##### SEND TO WEB SERVER ####
-#     df = spark.sql('select vader_polarity, textBlob_polarity, mymodel_polarity, process_date from cryptonews')
-#     df.show(5)
+    ## obtener datos del dataframe
+    ##### SEND TO WEB SERVER ####
+    df = spark.sql('select vader_polarity, textBlob_polarity, myModel_prediction, process_date from cryptonews')
+    df.show(5)
 
 
-#     data_to_sent['labels'] = df.select("process_date").toPandas().values.tolist()
-#     data_to_sent['vader'] = df.select("vader_polarity").toPandas().values.tolist()
-#     data_to_sent['textblob'] = df.select("textBlob_polarity").toPandas().values.tolist()
-#    # data_to_sent['mymodel'] = df.select("mymodel_polarity").toPandas().values.tolist()
+    data_to_sent['labels'] = df.select("process_date").toPandas().values.tolist()
+    data_to_sent['vader'] = df.select("vader_polarity").toPandas().values.tolist()
+    data_to_sent['textblob'] = df.select("textBlob_polarity").toPandas().values.tolist()
+    data_to_sent['mymodel'] = df.select("myModel_prediction").toPandas().values.tolist()
 
     
-#     r = requests.post('http://localhost:5000/puerta-enlace/setdatos', json={
-#         "data": data_to_sent
-#     })
+    r = requests.post('http://localhost:5000/puerta-enlace/setdatos', json={
+        "data": data_to_sent
+    })
     
 
 
